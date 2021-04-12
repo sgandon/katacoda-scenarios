6392778bedc93d5b1648970c5650047132407eb3
sgandon <sgandon@talend.com>
2021-04-12 17:03:48 +0200

    fixes helm 3 download url

--- helm/env-init.sh
+++ helm/env-init.sh
@@ -1,1 +1,1 @@
-ssh root@host01 "curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/; chmod 700 get_helm.sh; ./get_helm.sh"
+ssh root@host01 "curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3; chmod 700 get_helm.sh; ./get_helm.sh"
